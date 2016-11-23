c3e99718232405f01cd624bf8fa954af594dfbda
Aynur Galiev <aynurgaliev@Mac-mini-Aynur.local>
2016-11-24 00:06:20 +0300

    Initial Commit

---
+++ VK Online/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VK Online
+//
+//  Created by Aynur Galiev on 24.ноября.2016.
+//  Copyright © 2016 Aynur Galiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VK OnlineTests/VK_OnlineTests.swift
@@ -0,0 +1,36 @@
+//
+//  VK_OnlineTests.swift
+//  VK OnlineTests
+//
+//  Created by Aynur Galiev on 24.ноября.2016.
+//  Copyright © 2016 Aynur Galiev. All rights reserved.
+//
+
+import XCTest
+@testable import VK_Online
+
+class VK_OnlineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
